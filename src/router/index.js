d0d9f6baf42db4c524182016fad960a2b8439286
Yao Cheng <sz.yaocheng@gmail.com>
2017-02-02 21:40:09 +0800

    Async component attempt

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '../pages/Login.vue'
-import Campaign from '../pages/Campaign.vue'
-import Trade from '../pages/Trade.vue'
-import Verification from '../pages/Verification.vue'
-import Queue from '../pages/Queue.vue'
-import Booking from '../pages/Booking.vue'
-import Review from '../pages/Review.vue'
-import dashboard from '../pages/dashboard.vue'
-import Shop from '../pages/Shop.vue'
-import Demo from '../pages/Demo.vue'
+// import Login from '../pages/Login.vue'
+// import Campaign from '../pages/Campaign.vue'
+// import Trade from '../pages/Trade.vue'
+// import Verification from '../pages/Verification.vue'
+// import Queue from '../pages/Queue.vue'
+// import Booking from '../pages/Booking.vue'
+// import Review from '../pages/Review.vue'
+// import Dashboard from '../pages/dashboard.vue'
+// import Shop from '../pages/Shop.vue'
+// import Demo from '../pages/Demo.vue'
+
+// const Login = r => require.ensure([], () => r(require('../pages/Login.vue')))
+// const Campaign = r => require.ensure([], () => r(require('../pages/Campaign.vue'), 'business'))
+// const Trade = r => require.ensure([], () => r(require('../pages/Trade.vue'), 'business'))
+// const Verification = r => require.ensure([], () => r(require('../pages/Verification.vue'), 'business'))
+// const Queue = r => require.ensure([], () => r(require('../pages/Queue.vue'), 'business'))
+// const Booking = r => require.ensure([], () => r(require('../pages/Booking.vue'), 'business'))
+// const Review = r => require.ensure([], () => r(require('../pages/Review.vue'), 'business'))
+
+const Dashboard = r => require.ensure([], () => r(require('../pages/Dashboard.vue')))
+const Shop = r => require.ensure([], () => r(require('../pages/Shop.vue')))
+const Demo = r => require.ensure([], () => r(require('../pages/Demo.vue')))
+
+const Login = resolve => require(['../pages/Login.vue'], resolve)
+const Campaign = resolve => require(['../pages/Campaign.vue'], resolve)
+const Trade = resolve => require(['../pages/Trade.vue'], resolve)
+const Verification = resolve => require(['../pages/Verification.vue'], resolve)
+const Queue = resolve => require(['../pages/Queue.vue'], resolve)
+const Booking = resolve => require(['../pages/Booking.vue'], resolve)
+const Review = resolve => require(['../pages/Review.vue'], resolve)
 
 Vue.use(Router)
 
@@ -31,7 +51,7 @@ const router = new Router({
   }, {
     path: '/business/review', component: Review
   }, {
-    path: '/dashboard', component: dashboard
+    path: '/dashboard', component: Dashboard
   }, {
     path: '/shop', component: Shop
   }, {
